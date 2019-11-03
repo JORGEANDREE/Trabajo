d0d93e6bd4b0b84d8f279609b125f96811463d4d
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:04:26 -0500

    se agrego el 12vo ejericio

---
+++ ejercicio12.py
@@ -0,0 +1,14 @@
+#calcular la energia cinetica
+energia_cinetica,masa,velocidad=0.0,0.0,0.0
+
+#asignacion de valores
+masa=34
+velocidad=28
+
+#calculo
+energia_cinetica=(1/2)*masa*(velocidad*velocidad)
+
+#mostrar valores
+print("la masa es:",masa)
+print("la velocidad es:",velocidad)
+print("la energia_cinnetica es:",energia_cinetica)
