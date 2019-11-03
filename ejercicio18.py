a235a5df902261826594a3c233500a7f77248e67
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:09:51 -0500

    se agrego el 18vo ejericio

---
+++ ejercicio18.py
@@ -0,0 +1,15 @@
+#calcular el volumen de un paralepipedo recto
+volumen,a,b,h=0,0,0,0
+
+#asignacion de valores
+a=12
+b=5
+h=16
+
+#calculo
+volumen=a*b*h
+
+#mostrar valores
+print("la a es:",a)
+print("la b es:",b)
+print("el volumen es:",volumen)
