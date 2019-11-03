9d84d3a2d5f36b5998e0e44a62ae1b8aaee0c7d5
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:10:41 -0500

    se agrego el 19vo ejericio

---
+++ ejercicio19.py
@@ -0,0 +1,12 @@
+#calcular el volumen de un cubo
+arista,volumen=0,0.0
+
+#asignacion de valores
+arista=8
+
+#calculo
+volumen=arista*arista*arista
+
+#mostrar valores
+print("la arista:",arista)
+print("el volumen es:",volumen)
