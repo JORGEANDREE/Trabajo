948eaa0a1b90853f3db1369d08ef3a348ebf7d33
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:53:51 -0500

    se agrego el 3re ejericio

---
+++ ejercicio3.py
@@ -0,0 +1,16 @@
+#calcular el area de un trapecio
+area,base_menor,base_mayor,altura=0,0,0,0
+
+#asignacion de valores
+base_menor=40
+base_mayor=45
+altura=10
+
+#calculo
+area=(altura*(base_menor*base_mayor))/2
+
+#mostrar valores
+print("la base_menor es:",base_menor)
+print("la altura es:",altura)
+print("la base_mayor es:",base_mayor)
+print("el area es:",area)
