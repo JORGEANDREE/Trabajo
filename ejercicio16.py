e4a8f9d978d0ee08cefa63c3217d75c78d823696
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:08:11 -0500

    se agrego el 16vo ejericio

---
+++ ejercicio16.py
@@ -0,0 +1,14 @@
+#calcular la capacidad termica
+capacidad_termica,masa,calor_especifio=0.0,0.0,0.0
+
+#asignacion de valores
+masa=14
+calor_especifio=24
+
+#calculo
+capacidad_termica=masa*calor_especifio
+
+#mostrar valores
+print("la masa es:",masa)
+print("el calor_especifico es:",calor_especifio)
+print("la capacidad_termica es:",capacidad_termica)
