850204b35b69c27e147a1e9b20b6940d723ed93b
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:06:25 -0500

    se agrego el 14vo ejericio

---
+++ ejercicio14.py
@@ -0,0 +1,14 @@
+#calcular la presion
+area,fuerza,presion=0.0,0.0,0.0
+
+#asignacion de valores
+area=56.7
+fuerza=116.5
+
+#calculo
+presion=fuerza*area
+
+#mostrar valores
+print("la fuerza es:",fuerza)
+print("el area es:",area)
+print("la presion es:",presion)
