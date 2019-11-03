4caf22bad6ffabcb82217c895ba888b0a6e54699
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:03:31 -0500

    se agrego el 11vo ejericio

---
+++ ejercicio11.py
@@ -0,0 +1,12 @@
+#calcular el periodo(M.C.U)
+periodo,frecuencia=0.0,0.0
+
+#asignacion de valores
+frecuencia=12
+
+#calculo
+periodo=1/frecuencia
+
+#mostrar valores
+print("la frecuencia es:",frecuencia)
+print("el periodo es:",periodo)
