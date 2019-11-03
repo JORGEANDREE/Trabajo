bf8a52c396028b8c74eb96bf4ecc2ed706421531
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:48:38 -0500

    se agrego el primer ejericio

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+#calcular el area de un rectangulo
+area,base,altura=0,0,0
+
+#asignacion de valores
+base=30
+altura=40
+
+#calculo
+area=base*altura
+
+#mostrar valores
+print("la base es:",base)
+print("la altura es:",altura)
+print("el area es:",area)
