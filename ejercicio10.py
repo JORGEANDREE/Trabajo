af7e013003911cab10e7a9d1387f85deb1377665
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:02:13 -0500

    se agrego el 10mo ejericio

---
+++ ejercicio10.py
@@ -0,0 +1,12 @@
+#calcular el area de un cuadrado
+area,lado=0,0
+
+#asignacion de valores
+lado=18
+
+#calculo
+area=lado*lado
+
+#mostrar valores
+print("el lado es:",lado)
+print("el area es:",area)
