ae213b92b900e134ab6455e17bf4e727b63b4915
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:08:58 -0500

    se agrego el 17vo ejericio

---
+++ ejercicio17.py
@@ -0,0 +1,14 @@
+#calcular la potencia efecto joule
+potencia,resistencia_electrica,corriente=0.0,0.0,0.0
+
+#asignacion de valores
+resistencia_electrica=45
+corriente=13
+
+#calculo
+potencia=resistencia_electrica*(corriente*corriente)
+
+#mostrar valores
+print("la resistencia_electrica es:",resistencia_electrica)
+print("la corriente es:",corriente)
+print("la potencia es:",potencia)
