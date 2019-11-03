4d2af836a7d439b19e78216a3493179cd1853eac
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:58:39 -0500

    se agrego el 8vo ejericio

---
+++ ejercicio8.py
@@ -0,0 +1,14 @@
+#calcular la capacitamcia
+capacitancia,carga_electrica,diferencia_de_potencial=0.0,0.0,0.0
+
+#asignacion de valores
+carga_electrica=28
+diferencia_de_potencial=10
+
+#calculo
+capacitancia=carga_electrica/diferencia_de_potencial
+
+#mostrar valores
+print("la carga_electrica es:",carga_electrica)
+print("la diferencia_de_potencial es:",diferencia_de_potencial)
+print("la capacitancia es:",capacitancia)
