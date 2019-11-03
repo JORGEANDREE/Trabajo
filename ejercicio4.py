973c5f3fd64fafa7c87c3014682da76c59c8d810
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:54:55 -0500

    se agrego el 4to ejericio

---
+++ ejercicio4.py
@@ -0,0 +1,16 @@
+#calcular la posicion final
+posion_final,posion_inicial,velocidad,tiempo=0.0,0.0,0.0,0.0
+
+#asignacion de valores
+posion_inicial=34.6
+velocidad=20
+tiempo=12
+
+#calculo
+posion_final=posion_inicial+(velocidad*tiempo)
+
+#mostrar valores
+print("la pocicion_inicial es:",posion_inicial)
+print("la velocidad es:",velocidad)
+print("el tiempo es:",tiempo)
+print("la posicion_final es:",posion_final)
