5d29af4cfd9d7f4040f74e0f2636b70d61e912a4
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:05:30 -0500

    se agrego el 13vo ejericio

---
+++ ejercicio13.py
@@ -0,0 +1,14 @@
+#calcular el impulso
+impulso,fuerza,intervalo_tiempo=0.0,0.0,0.0
+
+#asignacion de valores
+fuerza=28
+intervalo_tiempo=12
+
+#calculo
+impulso=fuerza*intervalo_tiempo
+
+#mostrar valores
+print("la fuerza es:",fuerza)
+print("la intervalo_tiempo es:",intervalo_tiempo)
+print("el impulso es:",impulso)
