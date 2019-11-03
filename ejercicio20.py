d15a81e195cfda052c4cd11d0df24b74d43e00c2
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:11:38 -0500

    se agrego el 20vo ejericio

---
+++ ejercicio20.py
@@ -0,0 +1,14 @@
+#calcular el teorema de pitagoras
+a,b,c=0,0,0.0
+
+#asignacion de valores
+a=4
+b=3
+
+#calculo
+c=(a*a)+(b*b)
+
+#mostrar valores
+print("a es:",a)
+print("b es:",b)
+print("c es:",c)
