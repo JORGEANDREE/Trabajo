bd0c2e14f5185102bbab566f456a463ad8590c02
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:56:39 -0500

    se agrego el 6to ejericio

---
+++ ejercicio6.py
@@ -0,0 +1,14 @@
+#calcular el peso
+peso,masa,gravedad=0.0,0.0,0.0
+
+#asignacion de valores
+masa=30.7
+gravedad=9.8
+
+#calculo
+peso=masa*gravedad
+
+#mostrar valores
+print("la masa es:",masa)
+print("la gravedad es:",gravedad)
+print("el peso es:",peso)
