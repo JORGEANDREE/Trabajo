fd2334f9b6907869bd76e0b0827a5577143daa9b
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 18:07:19 -0500

    se agrego el 15vo ejericio

---
+++ ejercicio15.py
@@ -0,0 +1,16 @@
+#calcular el area de un rectangulo
+empuje,p,g,v=0,0,0,0
+
+#asignacion de valores
+p=45
+g=40
+v=16
+
+#calculo
+empuje=p*g*v
+
+#mostrar valores
+print("p es:",p)
+print("g es:",g)
+print("v es:",v)
+print("el empuje es:",empuje)
