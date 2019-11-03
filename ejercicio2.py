b36d9b171c721151531c9754fd6ded2863741745
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:51:45 -0500

    se agrego el 2do ejericio

---
+++ ejercicio2.py
@@ -0,0 +1,14 @@
+#calcular el area de un rombo
+area,diagonal_menor,digonal_mayor,=0.0,0,0
+
+#asignacion de valores
+diagonal_menor=45
+digonal_mayor=69
+
+#calculo
+area=(digonal_mayor*diagonal_menor)/2
+
+#mostrar valores
+print("la diagonal_menor es:",diagonal_menor)
+print("la diagonal_mayor es:",digonal_mayor)
+print("el area es:",area)
