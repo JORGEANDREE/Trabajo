8715ebc6a9349ad0a7dca99881c5050b6c3da5e8
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:57:41 -0500

    se agrego el 7mo ejericio

---
+++ ejercicio7.py
@@ -0,0 +1,14 @@
+#calcular el cantidad de movimiento
+movimiento,masa,velocidad=0.0,0.0,0.0
+
+#asignacion de valores
+masa=15.7
+velocidad=16
+
+#calculo
+movimiento=masa*velocidad
+
+#mostrar valores
+print("la masa es:",masa)
+print("la velocidad es:",velocidad)
+print("el movimiento es:",movimiento)
