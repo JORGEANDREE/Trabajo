118370068deac610c6eb5dde6a169987363b218a
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-03 17:55:45 -0500

    se agrego el 5to ejericio

---
+++ ejercicio5.py
@@ -0,0 +1,13 @@
+#calcular la velocidad (M.C.U)
+velocidad,velocidad_angular,radio=0.0,0.0,0.0
+
+#asignacion de valores
+velocidad_angular=10
+radio=4
+#calculo
+velocidad=velocidad_angular*radio
+
+#mostrar valores
+print("la velocidad _angular es:",velocidad_angular)
+print("el radio es:",radio)
+print("la velocidad es:",velocidad)
